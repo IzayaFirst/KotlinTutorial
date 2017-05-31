ac1cdf6c0e8e430050a886cc49a5d305cb61ad11
First <patcharaleelalumplert@gmail.com>
2017-06-01 00:27:12 +0700

    Test how to use if else and short if

--- src/Unit1/TestIfElse.kt
+++ src/Unit1/TestIfElse.kt
@@ -3,3 +3,11 @@ package Unit1
 /**
  * Created by user on 1/6/2560.
  */
+fun main(args : Array<String>) {
+    var x = 10
+    var y : Int = 12
+    if (x > y) println("x more than y") else println("x less than y")
+
+    if(x is Int) println(true) else println(false)
+}
+

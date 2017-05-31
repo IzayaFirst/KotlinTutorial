3c20d97c1459c4143c96e92c069b24bd076dfe9c
First <patcharaleelalumplert@gmail.com>
2017-06-01 00:13:33 +0700

    Learn how to  Assign Data with var and  val with Data type  String int double

--- src/Unit1/DataOperaion.kt
+++ src/Unit1/DataOperaion.kt
@@ -3,3 +3,16 @@ package Unit1
 /**
  * Created by user on 1/6/2560.
  */
+fun main(args: Array<String>) {
+    var x = 1
+    var y = 10
+    println("x+y = "+(x+y))
+    var name: String = "James"
+    val PI: Double = 3.14
+    println(name)
+    println(PI)
+    x = 12
+    var surname = "Dragoon"
+    println("Hello ${name + " "+ surname}")
+    
+}
